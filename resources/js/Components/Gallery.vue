3adac5e1d59a2da645fbf9b8b46c6cfd15bf5ba2
Nikita_Serik <nik-serik@mail.ru>
2021-10-17 01:31:52 +0300

    Edit Gallery

--- resources/js/Components/Gallery.vue
+++ resources/js/Components/Gallery.vue
@@ -3,9 +3,14 @@
         <div v-if="items && items.length > 0">
             <h1 class="font-merriweather text-3xl italic pb-3">Фотографии</h1>
 
-            <div class="grid grid-cols-4 gap-2">
+            <div class="grid grid-cols-3 gap-2">
                 <div v-for="item in items">
-                    <a :href="item.image" class="block aspect-w-4 aspect-h-3">
+                    <a
+                        data-fancybox="gallery"
+                        :data-caption="item.description"
+                        :href="item.image"
+                        class="block aspect-w-4 aspect-h-3"
+                    >
                         <img class="w-full object-cover" :src="item.image" :alt="item.description"/>
                     </a>
                 </div>
@@ -21,13 +26,18 @@
 
 <script>
 import { defineComponent } from 'vue'
+import { Fancybox } from "@fancyapps/ui"
+import "@fancyapps/ui/dist/fancybox.css"
 
 export default defineComponent({
     components: {
+        Fancybox
+    },
+    mounted() {
 
     },
     props: {
         items: Object
-    },
+    }
 })
 </script>
